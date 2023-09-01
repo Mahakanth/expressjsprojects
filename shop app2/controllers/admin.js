5d6d2a221920df175302c5d5e63060931c7ef60d
Mahakanth <mahakanth9@gmail.com>
2023-09-01 10:57:04 +0530

    defining a model

--- shop app2/controllers/admin.js
+++ shop app2/controllers/admin.js
@@ -14,12 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save()
-  .then(()=>{
-    res.redirect('/');
-  })
-  .catch(err =>{console.log(err);});
-  
+  product
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -70,9 +70,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-  .then(()=>{
-    res.redirect('/admin/products')
-  })
-  .catch(err=>console.log(err));
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
--- shop app2/util/database.js
+++ shop app2/util/database.js
@@ -1,10 +1,8 @@
-const mysql=require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool=mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'shopapp-server',
-    password: 'Lakki@6625'
-})
+const sequelize = new Sequelize('shopapp-server', 'root', 'Lakki@6625', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
-module.exports=pool.promise();
+module.exports = sequelize;
