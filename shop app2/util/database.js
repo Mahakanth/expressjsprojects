f38bf28d6ea64b22f88e7bdfb97f272952112485
Mahakanth <mahakanth9@gmail.com>
2023-09-03 12:09:15 +0530

    cart with many to many relationship

--- shop app2/util/database.js
+++ shop app2/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('shopapp-server', 'root', 'Lakki@6625', {
   dialect: 'mysql',
   host: 'localhost'
 });
